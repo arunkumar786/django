7f7e898c38b7ace18baedeba2c68a5fbedbe2b66
arun <techlinux4arun@gmail.com>
2017-06-05 23:16:03 +0530

    included modified models.py and views.py learnt for chapter 2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+# detail view
+def detail(request, question_id,):
+    return HttpResponse("you are looking at question %s" %question_id)
+
+# results view
+def results(request, question_id):
+    response = "you are looking at question %s."
+    return HttpResponse(response %question_id)
+
+# vote view.
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s" %question_id)
